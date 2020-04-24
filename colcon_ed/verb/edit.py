7e1edb8e847f28453289291001916a1d52d371e8
Chen Bainian <brian97cbn@gmail.com>
2020-04-24 20:49:46 +0800

    Add in custom file completer and editor function
    Signed-off-by: Chen Bainian <brian97cbn@gmail.com>

--- colcon_ed/verb/edit.py
+++ colcon_ed/verb/edit.py
@@ -4,11 +4,12 @@
 import argparse
 
 from colcon_core.package_discovery import add_package_discovery_arguments
-from colcon_core.package_discovery import discover_packages
-from colcon_core.package_identification \
-    import get_package_identification_extensions
 from colcon_core.plugin_system import satisfies_version
 from colcon_core.verb import VerbExtensionPoint
+from colcon_ed.edit_tools import edit_target_file
+from colcon_ed.edit_tools import find_file
+from colcon_ed.edit_tools import FileCompleter
+from colcon_ed.edit_tools import get_package_path
 
 
 class EditVerb(VerbExtensionPoint):
@@ -24,15 +25,46 @@ class EditVerb(VerbExtensionPoint):
             type=argument_package_name,
             help='Package name')
 
+        args = parser.add_argument(
+            'filename', nargs='?', metavar='FILE_NAME',
+            type=argument_package_name,
+            help='File name')
+        args.completer = FileCompleter(package_name_key='package_name')
+
         add_package_discovery_arguments(parser)
 
     def main(self, *, context):  # noqa: D102
         args = context.args
-        extensions = get_package_identification_extensions()
-        descriptors = discover_packages(args, extensions)
-        for pkg in descriptors:
-            if(pkg.name == args.package_name):
-                print(str(pkg.path))
+        package_path = get_package_path(args.package_name, args)
+        if not package_path:
+            return (("Package '{args.package_name}' " +
+                     'not found').format_map(locals()))
+
+        file_paths = find_file(package_path, args.filename)
+        if not file_paths:
+            return (("File '{args.filename}' not found " +
+                     'in package {args.package_name}').format_map(locals()))
+
+        # Ask the user to choose when there are more than one file found
+        if len(file_paths) > 1:
+            msg = 'Multiple files found:'
+            for i in range(len(file_paths)):
+                msg += '\n ' + str(i + 1) + ') ' + file_paths[i]
+            print(msg)
+            try:
+                index = input('Please enter a number: ')
+                index = int(index) - 1
+            except KeyboardInterrupt:
+                return '\n'
+            except ValueError:
+                return 'Not an integer'
+            if index < 0 or index >= len(file_paths):
+                return 'Index out of range'
+            path = file_paths[index]
+        else:
+            path = file_paths[0]
+
+        return edit_target_file(path)
 
 
 def argument_package_name(value):
